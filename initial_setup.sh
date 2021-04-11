cdabf47b35ddba89332552fa6dcd17465eea4e4a
Allan Charles <50716770+AChar95@users.noreply.github.com>
2021-04-11 02:18:13 +0100

    Corrected switch statement
    Corrected switch statement to remove the need for duplicated deployment function

--- initial_setup.sh
+++ initial_setup.sh
@@ -13,15 +13,14 @@ echo"-------------"
 echo "The following settings have been accepted: cluster name -> $clusterName, machine type -> $machineType, number of nodes -> $noNodes, zone -> $zone"
 echo "------------"
 echo "Are these values correct?"
-echo "Y/n"
-read $userResponse
+echo "Y/N"
+read $userResponse^^
 case $userResponse in
 	Y) echo "implementing the requested cluster size"
-		gcloud cluster containers create -m $machineType -num-nodes $noNodes --zone $zone $clusterName;
-	y) echo "Implementing the requested cluster size"
 		gcloud cluster containers create -m $machineType -num-nodes $noNodes --zone $zone $clusterName;
 	N) echo "which value is incorrect?"
 	   echo "1 -> clusterName, 2 -> machineType, 3 -> Number of Nodes, 4 -> zone, 5 -> all";
 	   read $userError
 	   case $userError in 
 		   1) 
+esac
